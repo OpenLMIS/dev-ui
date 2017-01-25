2be78fc546a0dcad55fa904c47f22a4ec76e8827
Nick Reid <nickreid@nickreid.com>
2017-01-24 20:03:50 -0800

    OLMIS-1072: Added build:openlmis.js task with hardcoded paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,9 @@
 module.exports = function(grunt){
-
 	grunt.loadTasks('./tasks');
+
+	grunt.option('buildDirectories', {
+		tmp: '.tmp',
+		build: 'build',
+		src: 'src'
+	})
 }
--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,48 +1,51 @@
 
 module.exports = function(grunt){
+    var fs = require('fs-extra'),
+    path = require('path'),
+    Concat = require('concat-with-sourcemaps'),
+    convertSourceMap = require('convert-source-map'),
+    wiredep = require('wiredep'),
+    glob = require('glob');
 
-	// Loop through each config folder
+    grunt.registerTask('build:openlmis.js', function(){
+        
+        var concat = new Concat(true, 'openlmis.js', '\n');
 
-	// Load app files in order - per config folder (pulling in wiredep things)
+        // Set general file patterns we want to ignore
+        var ignorePatterns = [
+            '**/*.spec.js', // don't include tests
+            'app.js', // should be generated
+            'app.routes.js' // should be generated
+        ];
+        // Helper function to keep ordered file adding clear
+        function addFiles(pattern){
+            glob.sync(pattern, {
+                cwd: path.join(process.cwd(), 'src/main/webapp'),
+                ignore: ignorePatterns
+            }).forEach(function(file){
+                var filePath = path.join(process.cwd(), 'src/main/webapp', file);
 
-	// Concat app files -- all openlmis.js
+                // Don't let previously added patterns be added again
+                ignorePatterns.push(pattern);
+                
+                var contentBuffer = fs.readFileSync(filePath);
+                concat.add(file, contentBuffer);
+            });
+        }
 
+        wiredep().js.forEach(function(file){
+            var contentBuffer = fs.readFileSync(file);
+            concat.add(file, contentBuffer);
+        });
 
-	       //    var appFiles = [
-        //     // Module registration
-        //     config.app.src + '/webapp/**/module/*.js',
-        //     config.app.src + '/webapp/**/*.module.js',
-        //     // Legacy files
-        //     config.app.src + '/webapp/js/shared/util.js',
-        //     config.app.src + '/webapp/js/shared/*.js',
-        //     config.app.src + '/webapp/js/shared/services/services.js',
-        //     config.app.src + '/webapp/js/shared/**/*.js',
-        //     // Special file types....
-        //     config.app.src + '/webapp/**/*.config.js',
-        //     config.app.src + '/webapp/**/*.routes.js',
-        //     '!' + config.app.src + '/webapp/app.routes.js',
-        //     // Everything else
-        //     config.app.src + '/webapp/**/*.js',
-        //     '!' + config.app.src + '/**/*.spec.js',
-        //     '!' + config.app.src + '/webapp/app.js',
-        //     '!' + config.app.src + '/webapp/app.routes.js',
-        //     // Run time
-        //     // NEED file to declare openlmis-app
-        //     config.app.src + '/webapp/app.js',
-        //     config.app.src + '/webapp/app.routes.js'
-        //   ];
-        //   // hack to make jquery load first
-        //   return [
-        //       'bower_components/jquery/dist/jquery.js',
-        //       'bower_components/jquery-ui/jquery-ui.js'
-        //     ].concat(
-        //       wiredep().js,
-        //       [
-        //         'vendor/ng-grid-2.0.7.min.js',
-        //         'vendor/base2.js'
-        //       ],
-        //       appFiles
-        //     );
-        // }(),
-        // dest: config.app.dest + '/webapp/openlmis.js'
+        addFiles('**/*.module.js');
+        addFiles('**/*.config.js');
+        addFiles('**/*.routes.js');
+        addFiles('**/*.js');
+
+        var inlineSourceMap = convertSourceMap.fromJSON(concat.sourceMap).toComment();
+        concat.add(null, inlineSourceMap);
+        
+        fs.writeFileSync('build/openlmis.js', concat.content);
+    });
 }
