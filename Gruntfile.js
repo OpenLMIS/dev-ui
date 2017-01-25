8e5128c16b09a7f6cb39fc0624fa2d6a959cd1e8
Nick Reid <nickreid@nickreid.com>
2017-01-25 12:40:41 -0800

    OLMIS-1072: Simplified set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,31 @@
 module.exports = function(grunt){
 	grunt.loadTasks('./tasks');
 
-	grunt.option('buildDirectories', {
-		tmp: '.tmp',
-		build: 'build',
-		src: 'src'
-	});
+	grunt.registerTask('default', [
+		'build:clean',
+    'build:bower',
+		'build:openlmis.js',
+		'build:openlmis.css'
+    ]);
 
-	grunt.registerTask('default', ['build:openlmis.js']);
+
+
+  // grunt.registerTask('serve', ['serve:proxy', 'connect:server']);
+
+  // var buildTasks = ['clean', 'ngtemplates', 'copy', 'concat', 'sass', 'replace', 'appcache'];
+  // var styleguideTasks = ['kssSetup', 'kss', 'copy:kssCopyAppAssets'];
+
+  // var fullBuildTasks = [].concat(buildTasks);
+  // if(grunt.option('production')) fullBuildTasks.push('uglify');
+  // if(!grunt.option('noTest') && !grunt.option('appOnly')) fullBuildTasks.push('karma:unit');
+  // if(!grunt.option('noStyleguide') && !grunt.option('appOnly')) fullBuildTasks = fullBuildTasks.concat(styleguideTasks);
+  // if(!grunt.option('noDocs') && !grunt.option('appOnly')) fullBuildTasks.push('ngdocs');
+
+  // grunt.registerTask('build', fullBuildTasks);
+
+  // grunt.registerTask('check', ['clean', 'jshint', 'sasslint']);
+
+  // grunt.registerTask('docs', ['build'].concat('ngdocs'));
+  // grunt.registerTask('styleguide', buildTasks.concat(styleguideTasks));
+
 }
