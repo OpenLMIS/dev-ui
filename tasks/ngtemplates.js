62f23e28bd701590251bcff8823c25b6ec6b4b5e
Nick Reid <nickreid@nickreid.com>
2017-01-26 09:35:27 -0800

    OLMIS-1072: Added ngtemplates

--- tasks/ngtemplates.js
+++ tasks/ngtemplates.js
@@ -1,15 +1,42 @@
+module.exports = function(grunt){
+    var fs = require('fs-extra'),
+    path = require('path'),
+    glob = require('glob');
 
+    var inEachAppDir = require('../ordered-application-directory');
+    var tmpDir = path.join(process.cwd(), '.tmp', 'html');
 
+    grunt.loadNpmTasks('grunt-angular-templates');
 
-    // ngtemplates: {
-    //   app: {
-    //     cwd:      config.app.src + '/webapp',
-    //     src:      ['**/**.html', '!index.html'],
-    //     dest:     '.tmp/templates.js',
-    //     options: {
-    //       module:   'openlmis-templates',
-    //       standalone: true,
-    //       concat:   'js',
-    //     }
-    //   }
-    // },
+    grunt.config('ngtemplates', {
+        app: {
+            cwd:      tmpDir,
+            src:      ['**/**.html', '!index.html'],
+            dest:     path.join(process.cwd(),'.tmp/js','templates.module.js'),
+            options: {
+              module:   'openlmis-templates',
+              standalone: true,
+              concat:   'js',
+            }
+        }
+    });
+
+    grunt.registerTask('copy:html', function(){
+        inEachAppDir(function(dir){
+            var src = 'src';
+            var config = grunt.file.readJSON(path.join(dir, 'config.json'));
+            if(config && config.app && config.app.src) src = config.app.src;
+
+            glob.sync('**/*.html', {
+                cwd: path.join(dir, src),
+                ignore: ['index.html'] // Don't copy the index.html file - its special
+            }).forEach(function(file){
+                fs.copySync(path.join(dir, src, file), path.join(tmpDir, file));
+            });
+        });
+    });
+
+    grunt.registerTask('build:html', function(){
+        grunt.task.run('copy:html', 'ngtemplates');
+    });
+}
