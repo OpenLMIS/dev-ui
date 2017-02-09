0997542c799490a2b3679dc23b9836f8099ba168
Nick Reid <nickreid@nickreid.com>
2017-02-09 13:25:01 -0800

    OLMIS-1585: Added key:value mapping to OPENLMIS_LANGUAGES

--- tasks/languages.js
+++ tasks/languages.js
@@ -5,21 +5,29 @@ module.exports = function(grunt){
     grunt.registerTask('languages', ['languages:make']);
 
     grunt.registerTask('languages:make', function(){
-        var tmpDir = path.join(process.cwd(), '.tmp', 'js');
+        var jsDir = path.join(process.cwd(), '.tmp', 'js');
+        var messagesDir = path.join(process.cwd(), 'src/main/resources/');
 
-        var languages = [];
+        var languages = {};
         glob.sync('messages*', {
-            cwd: 'src/main/resources/'
+            cwd: messagesDir
         }).forEach(function(filename){
             var fileLanguage = filename.substr(filename.lastIndexOf('.')-2, 2);
-            languages.push(fileLanguage);
+            var localeLabel = fileLanguage;
+
+            var languageObj = grunt.file.readJSON(path.join(messagesDir, filename));
+            if(languageObj && languageObj['locale.label']){
+                localeLabel = languageObj['locale.label'];
+            }
+            
+            languages[fileLanguage] = localeLabel;
         });
 
         var fileContents = '(function(){' + '\n';
         fileContents += 'angular.module("openlmis-config").constant("OPENLMIS_LANGUAGES", ' +  JSON.stringify(languages) + ');' + '\n';
         fileContents += '})();';
 
-        grunt.file.write(path.join(tmpDir, 'languages.js'), fileContents, {
+        grunt.file.write(path.join(jsDir, 'languages.js'), fileContents, {
             encoding: 'utf8'
         });
 
