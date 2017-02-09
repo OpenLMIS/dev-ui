b290dbf538646b345ca2683168d86463ec61d7ae
plal <pawellal@gmail.com>
2017-02-09 15:21:26 +0100

    OLMIS-1584: Added two message building processes (restored: transifex messages -> messages.js, added: multiple messages_en -> messages_en.json)

--- tasks/messages.js
+++ tasks/messages.js
@@ -1,28 +1,48 @@
 module.exports = function(grunt){
     var path = require('path'),
-    glob = require('glob'),
-    fs = require('fs-extra'),
-    extend = require('extend'),
-    eachAppDir = require('../ordered-application-directory.js');
+        glob = require('glob'),
+        fs = require('fs-extra'),
+        extend = require('extend'),
+        eachAppDir = require('../ordered-application-directory.js');
 
-    grunt.registerTask('messages', ['messages:make']);
+    grunt.registerTask('messages', ['messages:merge', 'messages:make']);
 
-    grunt.registerTask('messages:make', function(){
-        var tmpDir = path.join(process.cwd(), '.tmp', 'js');
+    grunt.registerTask('messages:merge', function(){ // step 1
+        var tmpDir = path.join(process.cwd(), '.tmp', 'messages');
         fs.emptyDir(tmpDir);
 
         var messages = {};
         eachAppDir(function(dir) {
-            dir += '/src/main/webapp/';
-
-            glob.sync('**/messages*', {
+            dir = path.join(dir, grunt.option('app.src'));
+            if(!fs.existsSync(dir))
+                return;
+            glob.sync('*/messages_en.json', {
                 cwd: dir
             }).forEach(function (filename) {
                 var filepath = path.join(dir, filename);
                 var messageObj = grunt.file.readJSON(filepath);
-                var fileLanguage = filename.substr(filename.lastIndexOf('.') - 2, 2);
-                messages[fileLanguage] = extend(true, messages[fileLanguage], messageObj);
+                messages = extend(true, messages, messageObj);
             });
+        });
+
+        grunt.file.write(path.join(tmpDir, 'messages_en.json'), JSON.stringify(messages), {
+            encoding: 'utf8'
+        });
+
+    });
+
+    grunt.registerTask('messages:make', function(){ // step 3
+        var tmpDir = path.join(process.cwd(), '.tmp', 'js');
+        fs.emptyDir(tmpDir);
+
+        var messages = {};
+        glob.sync('messages*', {
+            cwd: 'src/main/resources/'
+        }).forEach(function(filename){
+            var filepath = path.join(process.cwd(), 'src/main/resources', filename);
+            var messageObj = grunt.file.readJSON(filepath);
+            var fileLanguage = filename.substr(filename.lastIndexOf('.')-2, 2);
+            messages[fileLanguage] = messageObj;
         });
 
         var fileContents = '(function(){' + '\n';
