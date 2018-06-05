c8a080054c2d4114eb50abdd110b61cf97d1ac16
Jakub Kondrat <jkondrat@soldevelo.com>
2018-06-05 17:51:24 +0200

    OLMIS-4795: Fix equals for sh

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ TX_PUSH=${TRANSIFEX_PUSH:-true}
 TX_PULL=${TRANSIFEX_PULL:-true}
 
 # Built and test
-if [ "$TX_PUSH" == "true" ]; then
+if [ "$TX_PUSH" = "true" ]; then
   grunt --production --pullTransifex --pushTransifex
-elif [ "$TX_PULL" == "true" ]; then
+elif [ "$TX_PULL" = "true" ]; then
   grunt --production --pullTransifex
 else
   grunt --production
